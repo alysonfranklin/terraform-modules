ed4ad390eb88137d3e77e14953e7860f97557b16
Alyson Franklin <AlysonFranklinReal@gmail.com>
2020-06-11 14:24:49 -0300

    Adicionando condicional na criação do recurso schedule do ASG e adicionando módulo cloudwatch-log_group

---
+++ modules/cloudwatch-log_group/main.tf
@@ -0,0 +1,12 @@
+resource "aws_cloudwatch_log_group" "this" {
+  count = var.CREATE-LOG_GROUP == true ? 1 : 0
+
+  name = var.LOG_GROUP != "" ? var.LOG_GROUP : random_string.random.result
+  tags = var.DEFAULT_TAGS
+}
+
+resource "random_string" "random" {
+  length  = 16
+  upper   = false
+  special = false
+}
