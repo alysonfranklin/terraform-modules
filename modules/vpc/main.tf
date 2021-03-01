bea3e93118e4d4076f41b2d3155db34ce3123388
Alyson Franklin <AlysonFranklinReal@gmail.com>
2021-03-01 15:58:47 -0300

    Corrigindo o erro (multiple VPC Endpoint Services matched)

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -850,17 +850,13 @@ resource "aws_route" "private_nat_gateway" {
 ######################
 # VPC Endpoint para S3
 ######################
-data "aws_vpc_endpoint_service" "s3" {
-  count = var.create_vpc && var.enable_s3_endpoint ? 1 : 0
-
-  service = "s3"
-}
+data "aws_region" "current" {}
 
 resource "aws_vpc_endpoint" "s3" {
   count = var.create_vpc && var.enable_s3_endpoint ? 1 : 0
 
   vpc_id       = local.vpc_id
-  service_name = data.aws_vpc_endpoint_service.s3[0].service_name
+  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
 }
 
 resource "aws_vpc_endpoint_route_table_association" "private_s3" {
